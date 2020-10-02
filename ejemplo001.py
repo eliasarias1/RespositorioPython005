4db6350a25335631f5e6f6fd234d899e96ca8e03
eliasarias1 <elia.arias@alumnos.duoc.cl>
2020-10-02 13:06:19 -0300

    Se agrego un ejemplo simple en pyhton

---
+++ ejemplo001.py
@@ -0,0 +1,17 @@
+#ingresar dos numeros, mostrar la suma y la multiplicacion de ambos
+
+a=int(input("Digite un numero: "))
+b=int(input("Digite otro numero: "))
+suma=a+b
+multi=a*b
+print("La suma de de "+ str(a)+ "y de "+ str(b) + " Es igual a: " + str(suma))
+print("La multiplicacion de " + str(a) + " y de " + str(b) + " Es igual a: " +str(multi))
+
+#estructura if
+
+if(a>b):
+    print("El numero " + str(a)+ " Es mayor que: " + str(b))
+elif (a<b):
+    print("El numero " + str(b)+ " Es mayor que: " + str(a))
+else:
+    print("Los numeros son iguales!")
